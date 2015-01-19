fd645bf6d5a356e0a778bf6eb60808ad3296aed4
Jason Ozias <jason.ozias@ipaper.com>
2015-01-19 11:02:16 -0500

    getting travis to work

--- build.rs
+++ build.rs
@@ -12,15 +12,19 @@ fn main() {
     } else if libpath.join("libreadline.so").exists() {
         println!("cargo:rustc-flags=-l readline");
     } else {
+        let mut cflags = os::getenv("CFLAGS").unwrap_or(String::new());
         let src = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap());
         let dst = Path::new(os::getenv("OUT_DIR").unwrap()).join("build");
         let _ = fs::mkdir(&dst, io::USER_DIR);
+
+        cflags.push_str("-fPIC");
 
         run(Command::new("./configure")
             .cwd(&src.join("readline"))
             .arg("--disable-shared"));
 
         run(Command::new("make")
+            .env("CFLAGS", cflags.as_slice())
             .cwd(&src.join("readline")));
 
         let _ = fs::copy(&src.join("readline").join("libreadline.a"), &dst.join("libreadline.a"));
