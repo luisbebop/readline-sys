e0184f7a4eb184ee6971b290e44c6f1a6a62130d
Jason Ozias <jason.ozias@ipaper.com>
2015-02-04 10:15:04 -0500

    updated to work with rust nightly

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
-#![feature(core,env,io,path)]
-use std::env;
+#![feature(core,io,os,path)]
+use std::os;
 use std::old_io::{self, fs, Command};
 use std::old_io::fs::PathExtensions;
 use std::old_io::process::InheritFd;
 
 fn main() {
-    let target = env::var_string("TARGET").unwrap();
+    let target = os::getenv("TARGET").unwrap();
 
     let mingw = target.contains("windows-gnu");
 
@@ -25,12 +25,12 @@ fn main() {
         println!("cargo:rustc-flags=-l readline");
         println!("cargo:rustc-flags=-L {}", libpath.display());
     } else {
-        let src = Path::new(env::var_string("CARGO_MANIFEST_DIR").unwrap())
+        let src = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap())
             .join("readline");
-        let dst = Path::new(env::var_string("OUT_DIR").unwrap()).join("build");
+        let dst = Path::new(os::getenv("OUT_DIR").unwrap()).join("build");
         let _ = fs::mkdir(&dst, old_io::USER_DIR);
 
-        let cflags = env::var_string("CFLAGS").unwrap_or(String::new());
+        let cflags = os::getenv("CFLAGS").unwrap_or(String::new());
 
         if mingw {
             run(Command::new("sh")
