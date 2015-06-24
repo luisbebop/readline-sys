a02d24f94ccc8657cd2e0edaf129488e9301835e
Jason Ozias <jason.g.ozias@gmail.com>
2015-06-24 19:06:39 -0400

    build.rs

--- build.rs
+++ build.rs
@@ -4,20 +4,7 @@ use std::path::{Path,PathBuf};
 use std::process::{Command,Stdio};
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
-
-    let mingw = target.contains("windows-gnu");
-
-    let libpath = if mingw {
-        Path::new("C:/mingw64/bin")
-    } else {
-        Path::new("/usr/lib")
-    };
-
-    let dll = match fs::metadata(libpath.join("libreadline6.dll")) {
-        Ok(meta) => meta.is_file(),
-        Err(_)   => false,
-    };
+    let libpath = Path::new("/usr/lib");
 
     let statik = match fs::metadata(libpath.join("libreadline.a")) {
         Ok(meta) => meta.is_file(),
@@ -29,60 +16,38 @@ fn main() {
         Err(_)   => false,
     };
 
-    if mingw && dll {
-        println!("cargo:rustc-flags=-l readline6");
-        println!("cargo:rustc-flags=-L {}", libpath.display());
-    } else if !mingw && statik {
+    if statik {
         println!("cargo:rustc-flags=-l static=readline");
         println!("cargo:rustc-flags=-L {}", libpath.display());
-    } else if !mingw && dyn {
+    } else if dyn {
         println!("cargo:rustc-flags=-l readline");
         println!("cargo:rustc-flags=-L {}", libpath.display());
     } else {
-        let src = PathBuf::from(&env::var_os("CARGO_MANIFEST_DIR").unwrap())
-            .join("readline");
-        let dst = PathBuf::from(&env::var_os("OUT_DIR").unwrap()).join("build");
+        let manifest_dir = match env::var("CARGO_MANIFEST_DIR") {
+            Ok(md) => md,
+            Err(e) => panic!("manifest dir not defined! {}", e),
+        };
+
+        let out_dir = match env::var("OUT_DIR") {
+            Ok(od) => od,
+            Err(e) => panic!("manifest dir not defined! {}", e),
+        };
+        let src = PathBuf::from(manifest_dir).join("readline");
+        let dst = PathBuf::from(out_dir).join("build");
         let _ = fs::create_dir(&dst);
 
         let cflags = env::var("CFLAGS").unwrap_or(String::new());
 
-        if mingw {
-            run(Command::new("sh")
-                .arg("-c")
-                .arg("configure")
-                .current_dir(&src));
-        } else {
-            run(Command::new("./configure").current_dir(&src));
-        }
-
-        if mingw {
-            run(Command::new("sh")
-                .arg("-c")
-                .arg("make")
-                .env("CFLAGS", &cflags[..])
-                .current_dir(&src));
-        } else {
-            run(Command::new("make")
-                .env("CFLAGS", &cflags[..])
-                .current_dir(&src));
-        }
+        run(Command::new("./configure").current_dir(&src));
+        run(Command::new("make")
+            .env("CFLAGS", &cflags[..])
+            .current_dir(&src));
 
         let shlib = src.join("shlib");
-        if mingw {
-            let _ = fs::copy(&shlib.join("libreadline6.dll"),
-                             &dst.join("libreadline.dll"));
-            let _ = fs::copy(&shlib.join("libhistory6.dll"),
-                             &dst.join("libhistory.dll"));
-            let _ = fs::copy(&shlib.join("libreadline6.dll.a"),
-                             &dst.join("libreadline.dll.a"));
-            let _ = fs::copy(&shlib.join("libhistory6.dll.a"),
-                             &dst.join("libhistory.dll.a"));
-        } else {
-            let _ = fs::copy(&shlib.join("libreadline.so.6.3"),
-                             &dst.join("libreadline.so.6.3"));
-            let _ = fs::copy(&shlib.join("libhistory.so.6.3"),
-                             &dst.join("libhistory.so.6.3"));
-        }
+        let _ = fs::copy(&shlib.join("libreadline.so.6.3"),
+                         &dst.join("libreadline.so.6.3"));
+        let _ = fs::copy(&shlib.join("libhistory.so.6.3"),
+                         &dst.join("libhistory.so.6.3"));
 
         println!("cargo:rustc-flags=-l readline");
         println!("cargo:rustc-flags=-L {}", dst.display());
