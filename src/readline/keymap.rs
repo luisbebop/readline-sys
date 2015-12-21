30161ca6d6998268f7f251f60fc273fc4ff654a1
Jason Ozias <jason.g.ozias@gmail.com>
2015-12-21 14:58:29 -0500

    readline api work

--- src/readline/keymap.rs
+++ src/readline/keymap.rs
@@ -11,6 +11,12 @@ mod ext_keymap {
 
     extern "C" {
         pub fn rl_make_bare_keymap() -> Keymap;
+        pub fn rl_copy_keymap(map: Keymap) -> Keymap;
+        pub fn rl_make_keymap() -> Keymap;
+        pub fn rl_discard_keymap(map: Keymap) -> ();
+        pub fn rl_free_keymap(map: Keymap) -> ();
+        pub fn rl_get_keymap() -> Keymap;
+        pub fn rl_set_keymap(map: Keymap) -> ();
     }
 }
 
@@ -22,18 +28,133 @@ mod ext_keymap {
 /// ```
 /// use rl_sys::readline::keymap;
 ///
-/// let keymap = keymap::create_empty_keymap().unwrap();
+/// let keymap = keymap::create_empty().unwrap();
 /// assert!(!keymap.is_null());
 /// ```
-pub fn create_empty_keymap() -> Result<Keymap, ::ReadlineError> {
+pub fn create_empty() -> Result<Keymap, ::ReadlineError> {
     unsafe {
         let keymap_ptr = ext_keymap::rl_make_bare_keymap();
 
         if keymap_ptr.is_null() {
-            Err(::ReadlineError::new("Null Pointer", "Make bare keymap returned null pointer!"))
+            Err(::ReadlineError::new("Null Pointer", "rl_make_bare_keymap returned null pointer!"))
         } else {
-            println!("Keymap PTR: {:?}", keymap_ptr);
             Ok(keymap_ptr)
         }
     }
+}
+
+/// Return a new keymap which is a copy of map.
+///
+/// # Examples
+///
+/// ```
+/// use rl_sys::readline::keymap;
+///
+/// let keymap = keymap::create_empty().unwrap();
+/// assert!(!keymap.is_null());
+/// let km_copy = keymap::copy(keymap).unwrap();
+/// assert!(!km_copy.is_null());
+/// ```
+pub fn copy(map: Keymap) -> Result<Keymap, ::ReadlineError> {
+    unsafe {
+        let keymap_ptr = ext_keymap::rl_copy_keymap(map);
+
+        if keymap_ptr.is_null() {
+            Err(::ReadlineError::new("Null Pointer", "rl_copy_keymap returned null pointer!"))
+        } else {
+            Ok(keymap_ptr)
+        }
+    }
+}
+
+/// Return a new keymap with the printing characters bound to `rl_insert`, the lowercase Meta
+/// characters bound to run their equivalents, and the Meta digits bound to produce numeric
+/// arguments.
+///
+/// # Examples
+///
+/// ```
+/// use rl_sys::readline::keymap;
+///
+/// let keymap = keymap::make().unwrap();
+/// assert!(!keymap.is_null());
+/// ```
+pub fn make() -> Result<Keymap, ::ReadlineError> {
+    unsafe {
+        let keymap_ptr = ext_keymap::rl_make_keymap();
+
+        if keymap_ptr.is_null() {
+            Err(::ReadlineError::new("Null Pointer", "rl_make_keymap returned null pointer!"))
+        } else {
+            Ok(keymap_ptr)
+        }
+    }
+}
+
+/// Free the storage associated with the data in keymap. The caller should free keymap.
+///
+/// # Examples
+///
+/// ```
+/// use rl_sys::readline::keymap;
+///
+/// let keymap = keymap::make().unwrap();
+/// assert!(!keymap.is_null());
+/// keymap::discard(keymap);
+/// ```
+pub fn discard(map: Keymap) -> () {
+    unsafe { ext_keymap::rl_discard_keymap(map) }
+}
+
+/// Free all storage associated with keymap. This calls `rl_discard_keymap` to free subordindate
+/// keymaps and macros.
+///
+/// # Examples
+///
+/// ```
+/// use rl_sys::readline::keymap;
+///
+/// let keymap = keymap::make().unwrap();
+/// assert!(!keymap.is_null());
+/// keymap::free(keymap);
+/// ```
+pub fn free(map: Keymap) -> () {
+    unsafe { ext_keymap::rl_free_keymap(map) }
+}
+
+/// Returns the currently active keymap.
+///
+/// # Examples
+///
+/// ```
+/// use rl_sys::readline::keymap;
+///
+/// let keymap = keymap::get().unwrap();
+/// assert!(!keymap.is_null());
+/// ```
+pub fn get() -> Result<Keymap, ::ReadlineError> {
+    unsafe {
+        let keymap_ptr = ext_keymap::rl_get_keymap();
+
+        if keymap_ptr.is_null() {
+            Err(::ReadlineError::new("Null Pointer", "rl_get_keymap returned null pointer!"))
+        } else {
+            Ok(keymap_ptr)
+        }
+    }
+}
+
+/// Makes keymap the currently active keymap.
+///
+/// # Examples
+///
+/// ```
+/// use rl_sys::readline::keymap;
+///
+/// let keymap = keymap::get().unwrap();
+/// assert!(!keymap.is_null());
+/// keymap::set(keymap);
+/// ```
+pub fn set(map: Keymap) -> () {
+    unsafe { ext_keymap::rl_set_keymap(map) }
 }
